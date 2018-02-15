4fd50861b206fa80241e1d5004a2428eb061186a
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2018-02-15 22:22:47 +0530

    4Th Session about the pannels which acts like a containers to the elements which enables us to craete teh UI  from simpe to complax Designs.     Added the Pannels Folder for this demo here we are dealing with the Canvas which didn't have the docking concept so sometimes while designig the the elements will overlap so to solve that problemwe have the Panel.Zindex property.

---
+++ Wpf_PluralSight/Pannels and Layout System/Canvas_Test.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_PluralSight.Pannels_and_Layout_System
+{
+    /// <summary>
+    /// Interaction logic for Canvas_Test.xaml
+    /// </summary>
+    public partial class Canvas_Test : Page
+    {
+        public Canvas_Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
