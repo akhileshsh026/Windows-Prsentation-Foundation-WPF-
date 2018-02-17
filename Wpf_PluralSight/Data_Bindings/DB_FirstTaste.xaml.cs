3cd8fb26f6411027e908f003cbb7d41ffb592cb5
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2018-02-17 16:14:55 +0530

    Data Binding demo

---
+++ Wpf_PluralSight/Data_Bindings/DB_FirstTaste.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_PluralSight.Data_Bindings
+{
+    /// <summary>
+    /// Interaction logic for DB_FirstTaste.xaml
+    /// </summary>
+    public partial class DB_FirstTaste : Page
+    {
+
+        Person person = new Person();
+        public DB_FirstTaste()
+        {
+            InitializeComponent();
+            txtName.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(person.Name);
+        }
+    }
+}
